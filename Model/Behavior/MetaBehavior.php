46e9a786c0b85243559cc5f2c2d1691e7d7aba2d
Fabien Moreau <fmoreau.go@gmail.com>
2014-09-16 10:37:45 +0200

    PHPCS test
    Trim tabs blank space and add comments

--- Model/Behavior/MetaBehavior.php
+++ Model/Behavior/MetaBehavior.php
@@ -7,35 +7,35 @@
 class MetaBehavior extends ModelBehavior {
 
 	public $attribute = false;
-    
+
     public $schema = false;
-	
+
     public $callbackValues = array();
-    
+
     private $__currentJoins = array();
 
     /**
-     * MetaBehavior::setup()
-     * 
-     * @param Model $Model
-     * @param mixed $config
-     * @return boolean
-     */
-     
+    * MetaBehavior::setup()
+    * 
+    * @param Model $model
+    * @param array $config
+    * @return boolean
+    */
+
     public function setup(Model $model, $config = array()) {
-		# Loading Meta model to set find and save on the target table
-		$this->Meta = ClassRegistry::init("MetaBehavior.Meta");
-        # Storing current schema to compare with $this->data in beforeSave callback
+        // Loading Meta model to set find and save on the target table
+        $this->Meta = ClassRegistry::init("MetaBehavior.Meta");
+        // Storing current schema to compare with $this->data in beforeSave callback
 		$this->schema = array_keys($model->schema());
         return true;
     }
+
     /**
      * beforeFind Callback
      *
-     * @param Model $model Model find was run on
-     * @param array $results Array of model results.
-     * @param boolean $primary Did the find originate on $model.
-     * @return array Modified results
+     * @param Model $model Model find is being run on.
+     * @param array $query Array of Query parameters.
+     * @return array Modified query
      */
 
     public function beforeFind(Model $model, $query = array()) {
@@ -56,26 +56,34 @@ class MetaBehavior extends ModelBehavior {
 
         return $query;
     }
-    
+
+    /**
+     * Replace condition function
+     * Walk recursively to change meta value by join alias
+     * @param Model $model Model find is being run on.
+     * @param array $conditions Array of conditions query parameters.
+     * @return array Modified conditions
+     */
+
     private function __replace_conditions(Model $model, $conditions){
-    	
+
         foreach ($conditions as $cKey => $cValue) {
             if (is_array($cValue)) {
-                # If is array, we check recursively
+                // If is array, we check recursively
                 $conditions[$cKey] = $this->__replace_conditions($model, $cValue);
             }
 
             $match = array();
-            
-            # Get the field attach to the condition
-            $grep_field = preg_match("/".$model->alias."\.(\w+)/i", $cKey, $match);    
-            
+
+            // Get the field attach to the condition
+            $grep_field = preg_match("/".$model->alias."\.(\w+)/i", $cKey, $match);
+
             if ($grep_field) {
                 $field = $match[1];
-                
+
                 if (!in_array($field, $this->schema)) {
-                    
-                    # If the join is not set yet, we build it
+
+                    // If the join is not set yet, we build it
                     if (!array_key_exists($field, $this->__currentJoins)) {
                         $this->__currentJoins[$field] = array(
                             'table' => 'metas',
@@ -88,21 +96,21 @@ class MetaBehavior extends ModelBehavior {
                             )
                         );
                     }
-                    
-                    # Get the compare method
+
+                    // Get the compare method
                     $grep_operator = preg_match("/".$field."(.+)$/i", $cKey, $match);
-                    
+
                     if ($grep_operator) {
                         $operator = $match[1];
                     } else {
                         $operator = "";
                     }
-                    
-                    # Set new condition operator and delete the old one
-                    $conditions[$field . '.meta_value' . $operator] = $cValue;   
-                    
+
+                    // Set new condition operator and delete the old one
+                    $conditions[$field . '.meta_value' . $operator] = $cValue;
+
                     unset($conditions[$cKey]);
-                }    
+                }
             }
         }
 
@@ -117,45 +125,53 @@ class MetaBehavior extends ModelBehavior {
      * @param boolean $primary Did the find originate on $model.
      * @return array Modified results
      */
-     
-     public function afterFind(Model $model, $results, $primary = false) {
-     	
-        # Check if we're in a array of results
+
+    public function afterFind(Model $model, $results, $primary = false) {
+
+        // Check if we're in a array of results
      	if (!empty($results) && isset($results[0][$model->alias])) {
     		$primaryKeys = array();
      		$rangeResults = array();
-            
-            # Get all primaryKeys to send just one Meta request for one search
+
+            // Get all primaryKeys to send just one Meta request for one search
      		foreach ($results as $key => $result) {
      			$rangeResults[$result[$model->alias][$model->primaryKey]] = $result;
      			$primaryKeys[] = $result[$model->alias][$model->primaryKey];
      		}
-     		
-            # Get Metadatas
+
+            // Get Metadatas
      		$list = $this->Meta->find('all', array('conditions' => array(
             	'Meta.foreignModel =' => $model->alias,
         		'Meta.foreignKey' => $primaryKeys
         	)));
-            
-            # Sort results table with metas
+
+            // Sort results table with metas
         	foreach ($list as $l) {
         		$v = $l['Meta']['meta_value'];
             	$v = ($this->_is_serialized($v)) ? unserialize($v) : $v;
-            	$rangeResults[$l['Meta']['foreignKey']][$model->alias][$l['Meta']['meta_key']] = $v; 
+            	$rangeResults[$l['Meta']['foreignKey']][$model->alias][$l['Meta']['meta_key']] = $v;
         	}
-			
+
 			if ($model->findQueryType == 'first') {
 				return array(0 => array_shift($rangeResults));
 			}
-     		
-     		return $rangeResults; 	
+
+     		return $rangeResults;
      	}
-     	
+
      	return $results;
     }
 
+    /**
+     * beforeSave Callback
+     *
+     * @param Model $model Model find was run on
+     * @param array $options Array of saving options.
+     * @return boolean status for continue save action
+     */
+
     public function beforeSave(Model $model, $options = array()) {
-        # Storing data to save, compare to model schema, needed by afterSave callback
+        // Storing data to save, compare to model schema, needed by afterSave callback
         foreach ($model->data[$model->alias] as $row => $value) {
             if (!in_array($row, $this->schema)) {
                 $this->registerAttribute($model, $row, $value);
@@ -163,6 +179,15 @@ class MetaBehavior extends ModelBehavior {
         }
         return true;
     }
+
+    /**
+     * registerAttribute function
+     * Convert data to persist
+     * @param Model $model Model find was run on
+     * @param string $row string with meta key.
+     * @param mixed $value to store.
+     * @return boolean result
+     */
 
     public function registerAttribute(Model $model, $row, $value) {
 		if ($value != "" && is_array($value)) {
@@ -173,6 +198,17 @@ class MetaBehavior extends ModelBehavior {
 
         return true;
     }
+
+    /**
+     * setAttribute function
+     * Prepare datas to persist
+     * @param Model $model Model find was run on
+     * @param integer $foreignKey of the current model.
+     * @param string $key meta identifier.
+     * @param string $value meta to store
+     * @param boolean $primaryKey if add/editing value
+     * @return array attribute to persist
+     */
 
     public function setAttribute(Model $model, $foreignKey, $key, $value, $primaryKey = false) {
 
@@ -186,46 +222,70 @@ class MetaBehavior extends ModelBehavior {
         $attr['Meta']['foreignModel'] = $model->alias;
         $attr['Meta']['meta_key'] = $key;
         $attr['Meta']['meta_value'] = $value;
-        
+
         return $attr;
     }
 
+    /**
+     * afterSave callback
+     * Transaction to database
+     * @param Model $model Model find was run on
+     * @param boolean $created if add/edit
+     * @param array $options for saving configuration
+     * @return void
+     */
 
     public function afterSave(Model $model, $created, $options = array()) {
         if (!empty($this->callbackValues)) {
         	$many = array();
-        	
+
         	$previous_values = false;
-        	
+
         	if (!$created) {
         		$previous_values = $this->Meta->find('list',array(
         			'conditions' => array('Meta.foreignModel =' => $model->alias, 'Meta.foreignKey =' => $model->id),
         			'fields' => array('meta_key','id')
         		));
         	}
-        
+
             foreach ($this->callbackValues as $key => $value) {
             	$primary = false;
-            	
+
             	if (is_array($previous_values) && array_key_exists($key, $previous_values)) {
             		$primary = $previous_values[$key];
             	}
-            	
+
                 $many[] = $this->setAttribute($model, $model->id, $key, $value, $primary);
             }
-            
-            # Send transaction to meta table
+
+            // Send transaction to meta table
             $this->Meta->saveMany($many);
         }
-        
+
         $this->CallbackValues = array();
     }
+
+    /**
+     * afterDelete callback
+     * Delete associate values of the primary model
+     * @param Model $model Model find was run on
+     * @return void
+     */
 
     public function afterDelete(Model $model) {
         $this->Meta->deleteAll(array('Meta.foreignKey =' => $model->id, 'Meta.foreignModel =' => $model->alias));
     }
 
-    protected function _is_serialized( $data, $strict = true ) {
+    /**
+    * _is_serialized function (protected)
+    * Utility function to check if a string is serialized without generating exceptions
+    * @param Model $model Model find was run on
+    * @param mixed $data to check
+    * @param boolean $strict to set the mode
+    * @return boolean result
+    */  
+
+    protected function _is_serialized(Model $model, $data, $strict = true ) {
         if ( ! is_string( $data ) ) {
             return false;
         }
